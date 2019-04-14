dc7dc12ddc0ad22871d20496d422b9844723dffa
Tyler Thomas <alogia@gmail.com>
2019-04-14 09:46:36 -0500

    Added dot-setup options for wayland apps.

--- zsh/zsh_exports.sh
+++ zsh/zsh_exports.sh
@@ -1,6 +1,10 @@
 #General exports for all shells
 
 export GEM_HOME="$HOME/gems"
-export PATH="$PATH:$HOME/bin:$HOME/devel/bin:$(ruby -e 'print Gem.user_dir')/bin:$HOME/.local/bin/"
+export PATH="$PATH:$HOME/bin:$HOME/devel/bin:$(ruby -e 'print Gem.user_dir')/bin:$HOME/.local/bin/:$HOME/.cabal/bin"
 export QUOTING_STYLE=literal
 export EDITOR=vim
+
+export QT_QPA_PLATFORM=wayland-egl
+export CLUTTER_BACKEND=wayland
+export SDL_VIDEODRIVER=wayland
