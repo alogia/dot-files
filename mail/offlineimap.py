8a2b6822216b3d4f84def9eb0a33e49476a792ee
Tyler Thomas <alogia@gmail.com>
2019-04-22 09:48:09 -0500

    Removed all old oauth functions and decided on a non encrypted format for authfile info. Authfile now is just set to 0600 and is plaintext.

--- mail/offlineimap.py
+++ mail/offlineimap.py
@@ -1,36 +1,17 @@
 #!/usr/bin/env python
+
 import re
-from subprocess import check_output
+import os
+#from subprocess import check_output
 
 # The gmailauth file must be in the format:
 # login XXXX clientid XXXX secret XXXX refresh XXXX
-
-#goauthinfo = os.popen("gpg -q --no-tty -d ~/.gmailauth.gpg").read()
-
-
-
-def construct_re(login):
-    s = "login %s clientid ([^ ]+) secret ([^ ]+) refresh ([^ ]+)\n" % (login)
-    return re.compile(s)
-
-
-def get_oath_clientid(login):
-    p = construct_re(login)
-    return p.search(goauthinfo).group(1)
-
-
-def get_oath_secret(login):
-    p = construct_re(login)
-    return p.search(goauthinfo).group(2)
-
-
-def get_oath_refresh(login):
-    p = construct_re(login)
-    return p.search(goauthinfo).group(3)
 
 
 def get_authinfo_password(machine, login, port):
     s = "machine %s login %s password ([^ ]*) port %s" % (machine, login, port)
     p = re.compile(s)
-    authinfo = check_output("gpg -dq ~/.authinfo.gpg", shell=True)
+    #Use pgp encryption if desired.
+    #authinfo = check_output("gpg -dq ~/.authinfo.gpg", shell=True)
+    authinfo = open(os.path.expanduser("~/.authinfo")).read()
     return p.search(authinfo).group(1)
