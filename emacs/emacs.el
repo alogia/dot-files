0b30fd8f15afd6d6bae78bfddf94672bcd2a0ac2
Tyler Thomas <alogia@gmail.com>
2019-04-23 11:07:39 -0500

    Added support for Eshell
    Eshell now has a (use-package) declaration and some helper defuns which make it
    more useful.
    
    Bindings have been set to open or close eshell on C-!.
    A global variable my:eshell-visual-commands holds a list of names of all the
    programs which will need to be run in an ansi-term.
    
    dot-setup has been modified to support an eshell/alias file. This will install
    to ~/.emacs.d/eshell/alias.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -4,6 +4,9 @@
 (defvar my:plugin-dir  "~/.emacs.d/plugins")
 ;; Should emacs use a compiled init file?
 (defvar my:compiled-init nil)
+;;; Define the commands which will be run in an ansi-term instead of eshell
+(defvar my:eshell-visual-commands '("ssh" "tail" "htop"))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Org Mode setup
@@ -243,6 +246,103 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Eshell functions and key bindings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+(global-set-key (kbd "C-!")  'eshell-here)
+
+(defun eshell/x ()
+  "Exits an eshell buffer and closes the window."
+  (interactive)
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+
+(defun eshell/find (filename &optional dir try-count)
+  "Searches for files matching FILENAME in either DIR or the
+current directory. Just a typical wrapper around the standard
+`find' executable.
+
+Since any wildcards in FILENAME need to be escaped, this wraps the shell command.
+
+If not results were found, it calls the `find' executable up to
+two more times, wrapping the FILENAME pattern in wildcat
+matches. This seems to be more helpful to me."
+  (let* ((cmd (concat
+               (executable-find "find")
+               " " (or dir ".")
+               "      -not -path '*/.git*'"
+               " -and -not -path '*node_modules*'"
+               " -and -not -path '*classes*'"
+               " -and "
+               " -type f -and "
+               "-iname '" filename "'"))
+         (results (shell-command-to-string cmd)))
+
+    (if (not (s-blank-str? results))
+        results
+      (cond
+       ((or (null try-count) (= 0 try-count))
+        (eshell/f (concat filename "*") dir 1))
+       ((or (null try-count) (= 1 try-count))
+        (eshell/f (concat "*" filename) dir 2))
+       (t "")))))
+
+
+(defun eshell/ef (filename &optional dir)
+  "Searches for the first matching filename and loads it into a
+file to edit."
+  (let* ((files (eshell/f filename dir))
+         (file (car (s-split "\n" files))))
+    (find-file file)))
+
+(defun eshell/clear ()
+  "clear the eshell buffer."
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)))
+
+(use-package eshell
+  :init
+  (setenv "PATH"
+        (concat
+         "/usr/local/bin:/usr/local/sbin:" ;TODO: Add all the proper paths in here
+         (getenv "PATH")))
+  
+  :custom
+  (eshell-scroll-to-bottom-on-input 'all)
+  (eshell-error-if-no-glob t)
+  (eshell-hist-ignoredups t)
+  (eshell-save-history-on-exit t)
+  (eshell-prefer-lisp-functions nil)
+  (eshell-destroy-buffer-when-process-dies t)
+  
+  :hook
+  (eshell-mode . (lambda ()
+                   ;; Must define keymap in mode hook
+                   (define-key eshell-mode-map (kbd "C-!") 'eshell/x)
+                   (append 'my:eshell-visual-commands 'eshell-visual-commands)))
+  )
+                   
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Automatically compile and save ~/.emacs.el
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun byte-compile-init-files (file)
@@ -340,12 +440,14 @@
  ("C-c o"   . org-open-main)
  ("C-/"     . undo)
  ("C-x M-f" . project-find-file)
- ("C-c i"   . ielm))
+ ("C-x C-x" . kill-buffer-and-window)
+ )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Global Key Bindings Which can be overridden by minor modes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key (kbd "C-c v") 'visual-line-mode)
+(global-set-key (kbd "C-c i") 'ielm)
 
 
 ;; Unbind C-z from suspend-frame
