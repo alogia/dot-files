cc91f53a9e5548a69e4a18aeb1aed56fc09583ab
Tyler Thomas <alogia@gmail.com>
2019-04-24 19:35:26 -0500

    Added support for fzf
    Sourced the fzf required zsh files and exported some fzf options.

--- zsh/zsh_exports.sh
+++ zsh/zsh_exports.sh
@@ -4,8 +4,14 @@ export GEM_HOME="$HOME/gems"
 export PATH="$PATH:$HOME/bin:$HOME/devel/bin:$(ruby -e 'print Gem.user_dir')/bin:$HOME/.local/bin/:$HOME/.cabal/bin"
 export QUOTING_STYLE=literal
 export EDITOR=vim
+export VISUAL=vim
 
 export XCURSOR_SIZE=64
 export QT_QPA_PLATFORM=wayland-egl
 export CLUTTER_BACKEND=wayland
 export SDL_VIDEODRIVER=wayland
+
+#FZF setup options
+export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
+export FZF_DEFAULT_OPTS='--bind=alt-n:up,alt-t:down'
+export FZFZ_EXCLUDE_PATTERN='\.(git|cache|stack|mozilla)|node_modules'
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -9,11 +9,12 @@ antigen bundle git
 antigen bundle screen
 antigen bundle bundler
 antigen bundle tmux
-
+antigen bundle z
 
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search
 antigen bundle joel-porquet/zsh-dircolors-solarized.git
+antigen bundle andrewferrier/fzf-z
 
 
 antigen theme af-magic
@@ -32,7 +33,13 @@ source $HOME/.keybindings
 ############################################
 #			 	TWEEKS 
 ############################################
-
 #Turn off flow control.
 stty -ixon 
 unsetopt autopushd
+
+############################################
+#Set up fzf
+############################################
+source /usr/share/fzf/completion.zsh
+source /usr/share/fzf/key-bindings.zsh
+source /usr/share/fzf/fzf-extras.zsh
