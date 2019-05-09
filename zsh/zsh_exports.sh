aeb1cc485ea9d5329ee95826943739b7c4a2c554
Tyler Thomas <alogia@gmail.com>
2019-05-09 14:00:06 -0500

    Added wayland export flag for mozilla

--- zsh/zsh_exports.sh
+++ zsh/zsh_exports.sh
@@ -6,7 +6,7 @@ export QUOTING_STYLE=literal
 export EDITOR=vim
 export VISUAL=vim
 
-export XCURSOR_SIZE=64
+export XCURSOR_SIZE=16
 export QT_QPA_PLATFORM=wayland-egl
 export CLUTTER_BACKEND=wayland
 export SDL_VIDEODRIVER=wayland
@@ -15,3 +15,5 @@ export SDL_VIDEODRIVER=wayland
 export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
 export FZF_DEFAULT_OPTS='--bind=alt-n:up,alt-t:down'
 export FZFZ_EXCLUDE_PATTERN='\.(git|cache|stack|mozilla)|node_modules'
+
+export MOZ_ENABLE_WAYLAND=1
