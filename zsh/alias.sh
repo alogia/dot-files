e40d4f8305b9b5a27724f565b32c36b48032fcf5
Tyler Thomas <alogia@gmail.com>
2019-07-20 12:52:03 +0700

    Added dh command to refresh dhcp request.

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,2 +1,3 @@
-alias wifi='sudo netctl start "$(netctl list | fzf | sed -r '\''s/^[\* ]? //'\'')"'
+alias wifi='sudo netctl switch-to "$(netctl list | fzf | sed -r '\''s/^[\* ]? //'\'')"'
 alias als='alias | grep'
+alias dh='sudo dhcpcd wlp58s0'
