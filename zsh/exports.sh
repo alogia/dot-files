0368abdc50c9ed48de66ffa5a08bb9baca467f74
Tyler Thomas <alogia@gmail.com>
2020-07-30 15:36:14 -0600

    Added R config

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -11,13 +11,20 @@ export PATH="$PATH:$HOME/bin:$HOME/devel/bin:$(ruby -e 'print Gem.user_dir')/bin
 export EDITOR=vim
 export VISUAL=vim
 
+#Tex variables
+export PDFVIEWER=evince
+export TEXDOCVIEW_pdf=evince
+export TEXDOC_VIEWER_PDF=evince
+
 ## Wayland support flags
+export GDK_BACKEND=wayland
 export XCURSOR_SIZE=16
 export QT_QPA_PLATFORM=wayland-egl
 export CLUTTER_BACKEND=wayland
 export SDL_VIDEODRIVER=wayland
 export MOZ_ENABLE_WAYLAND=1
-export QT_SCALE_FACTOR=2
+#export QT_SCALE_FACTOR=2
+export QT_QPA_PLATFORMTHEME=qt5ct
 
 ## Misc
 export QUOTING_STYLE=literal
